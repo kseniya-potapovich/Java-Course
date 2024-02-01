5ec9ac489d7926adf19ca48408a51e4552d87b10
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-02-01 13:45:11 +0300

    Seventh commit "add proxy"

---
+++ GoF/src/main/java/proxy/Main.java
@@ -0,0 +1,11 @@
+package proxy;
+
+public class Main {
+    public static void main(String[] args) {
+        Project example1 = new RealProject("https://refactoring.guru/design-patterns/proxy/java/example");
+        example1.run();
+
+        Project example2 = new ProxyProject("https://refactoring.guru/design-patterns/proxy/java/example");
+        example2.run();
+    }
+}
