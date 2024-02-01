bae715a26dfddf9c0397481c58bc63f0074a4053
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-02-01 13:44:50 +0300

    Sixth commit "check prototype"

--- GoF/src/main/java/prototype/Main.java
+++ GoF/src/main/java/prototype/Main.java
@@ -3,13 +3,19 @@ package prototype;
 public class Main {
     public static void main(String[] args) {
         Human original = new Human("Pavel", 18);
-        System.out.println("First human: ");
+        System.out.println("First human:");
         System.out.println(original);
 
         Human clone = (Human) original.copy();
-        System.out.println("Clone: ");
+        System.out.println("Clone:");
         System.out.println(clone);
 
+        System.out.println("Check:");
         System.out.println(original.equals(clone));
+
+        Car car = new Car("Black", 200, "BMW");
+        System.out.println(car);
+        Car car1 = (Car) car.copy();
+        System.out.println(car1);
     }
 }
