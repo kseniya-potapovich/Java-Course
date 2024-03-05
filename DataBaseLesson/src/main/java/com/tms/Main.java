d8e0dd5bcbd97a44a375ecfa13a896f15c2c9f0d
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-03-05 21:39:17 +0300

    connect Spring from Java class

--- DataBaseLesson/src/main/java/com/tms/Main.java
+++ DataBaseLesson/src/main/java/com/tms/Main.java
@@ -1,24 +1,17 @@
 package com.tms;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
+        /** by Java */
         /** 1. Create Spring Container(ApplicationContext) */
-        ApplicationContext context = new ClassPathXmlApplicationContext("settings.xml"); // если настраиваем через xml
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         Cap capBean = (Cap) context.getBean("cap");
-        Cap capBean2 = (Cap) context.getBean("cap");
         System.out.println(capBean);
-        System.out.println(capBean2);
-        System.out.println(capBean.hashCode());
-        System.out.println(capBean2.hashCode());
-        capBean2.setText("Hello!!!!");
-        System.out.println(capBean);
-        System.out.println(capBean2);
-        System.out.println(capBean.hashCode());
-        System.out.println(capBean2.hashCode());
-
     }
 }
