64b1eb63cfc946c85be127504b2efe94a6f7eb34
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-04-04 20:32:11 +0300

    8th commit "Multipart"

---
+++ spring-boot/src/main/java/com/boot/springboot/controller/FileController.java
@@ -0,0 +1,53 @@
+package com.boot.springboot.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    private final Path ROOT_FILE_PATH = Paths.get("data");
+
+    @PostMapping("/upload")
+    public ResponseEntity<HttpStatus> upload(@RequestParam("file") MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), ROOT_FILE_PATH.resolve(file.getOriginalFilename()));
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    @GetMapping("/{filename}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Path path = ROOT_FILE_PATH.resolve(filename);
+        try {
+            Resource resource = new UrlResource(path.toUri());
+            if (resource.exists()) {
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"");
+                return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+            }
+        } catch (MalformedURLException e) {
+            System.out.println(e);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
